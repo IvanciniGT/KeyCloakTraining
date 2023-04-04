b026dfd767a19ee429a7631716f7e1d7a8e18f2d
Ubuntu <ubuntu@ip-172-31-45-169.eu-west-1.compute.internal>
2023-04-04 18:55:18 +0000

    Basic installation

---
+++ backend/README.md
@@ -0,0 +1,11 @@
+ mvn package spring-boot:run
+ 
+ In order to start the backend web service!
+ 
+ curl http://localhost:8086/test/anonymous
+ 
+ curl http://localhost:8086/test/user
+ 
+ curl http://localhost:8086/test/admin
+ 
+ 
---
+++ client/src/KeycloakLogin.js
@@ -0,0 +1,69 @@
+import React, { Component } from 'react'
+import Keycloak from "keycloak-js"
+import decode from 'jwt-decode'
+import JSONPretty from 'react-json-pretty'
+
+const Config = { // Mapa de JAVA
+    url: 'https://54.154.167.207:8443',
+    realm: 'prueba',
+    clientId: 'react',
+    onLoad: 'login-required',
+    scope: 'telefono'
+}
+
+class KeycloakLogin extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = { keycloak: null, authenticated: false};
+                                     // ^ Contiene la respuesta que nos da Keycloak cuando nos logueamos (TOKEN)
+                    // ^ Contiene información de la CONEXION a nuestro KEYCLOAK
+    }
+
+    componentDidMount() {
+        const keycloak = Keycloak(Config);
+        keycloak.init({onLoad: 'login-required'}).then(authenticated => {
+            this.setState({ keycloak: keycloak, authenticated: authenticated })
+            
+            
+            fetch("http://54.154.167.207:8086/test/admin", {
+            headers: {
+              "Authorization": "Bearer "+keycloak.token,
+            }})
+            .then(      (response) => response.text()        )
+            .then((text) => console.log(text))
+            .catch(error => console.log(error));
+            
+            
+            
+            
+            fetch("http://54.154.167.207:8086/test/user", {
+            headers: {
+              "Authorization": "Bearer "+keycloak.token,
+            }})
+            .then((response) => response.text())
+            .then((text) => console.log(text))
+            .catch(error => console.log(error));
+
+        });
+    }
+
+    render() {
+        if (this.state.keycloak && this.state.authenticated ) {
+            var jwt = JSON.stringify(decode(this.state.keycloak.token));
+            return <div  style={ {"marginLeft": "10%"} }>
+                        <div>
+                        <JSONPretty id="json-pretty" data={jwt}></JSONPretty>
+                        </div>
+                        <div style={ {"wordBreak": "break-all","width": "80%"} }>
+                        {this.state.keycloak.token}
+                        </div>
+                    </div>
+        } else {
+            return <div>Esperando a login de KeyCloak</div>
+        }
+    }
+
+}
+
+export default KeycloakLogin
